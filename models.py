2f44e07f49e583a85fec21c1ddec303dd63cc11e
Steven Schreiber <sschreiber.ssteven@gmail.com>
2020-02-17 17:53:01 -0800

    moving django files into testing branch

--- models.py
+++
@@ -1,8 +0,0 @@
-from google.appengine.ext import ndb
-
-class restaurant(ndb.Model):
-    title= ndb.StringProperty(required=True)
-    dish= ndb.GenericProperty(required=True)
-    res_img= ndb.GenericProperty(required=True)
-    dish_title= ndb.StringProperty(required=True)
-    foo= ndb.StringProperty(required=True)
